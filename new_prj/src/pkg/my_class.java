da54c17dd0a80ae994caeab09c1c524232d08210
悦民 <@192.168.3.6>
2018-05-26 19:51:27 +0900

    6.1コミット

--- new_prj/src/pkg/my_class.java
+++ new_prj/src/pkg/my_class.java
@@ -6,6 +6,9 @@ public class my_class {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("hello world");
 		System.out.println("hello world");
+
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
