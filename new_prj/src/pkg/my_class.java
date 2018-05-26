714aeb3192014c18c41896ab557a49ce1f572c13
悦民 <@melch>
2018-05-27 00:10:54 +0900

    5.9コミット

--- new_prj/src/pkg/my_class.java
+++ new_prj/src/pkg/my_class.java
@@ -15,7 +15,10 @@ public class my_class {
 
 		System.out.println("hello world");
 		System.out.println("hello world");
-		
+
+		System.out.println("hello world");
+		System.out.println("hello world");
+
 		System.out.println("hello world");
 		System.out.println("hello world");
 	}
