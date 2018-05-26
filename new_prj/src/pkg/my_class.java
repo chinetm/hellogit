009a8cbeb450b9221812635fe2c34befacd8f8df
悦民 <@melch>
2018-05-26 22:01:26 +0900

    5.4コミット

--- new_prj/src/pkg/my_class.java
+++ new_prj/src/pkg/my_class.java
@@ -7,6 +7,8 @@ public class my_class {
 		System.out.println("hello world");
 		System.out.println("hello world");
 		System.out.println("hello world");
+		
+		System.out.println("hello world");
 	}
 
 }
