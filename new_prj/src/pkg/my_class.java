7af27f844b77fb473501098311762be0cf64edf6
悦民 <@192.168.3.6>
2018-05-26 23:07:05 +0900

    6.7コミット

--- new_prj/src/pkg/my_class.java
+++ new_prj/src/pkg/my_class.java
@@ -12,7 +12,10 @@ public class my_class {
 		System.out.println("hello world");
 		System.out.println("hello world");
 		System.out.println("hello world");
-		
+
+		System.out.println("hello world");
+		System.out.println("hello world");
+
 		System.out.println("hello world");
 		System.out.println("hello world");
 	}
