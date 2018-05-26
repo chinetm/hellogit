e8109e8848b02013d304fca2c2862a8be5658bd9
悦民 <@192.168.3.6>
2018-05-26 22:12:52 +0900

    6.6 コミット

--- new_prj/src/pkg/my_class.java
+++ new_prj/src/pkg/my_class.java
@@ -7,6 +7,12 @@ public class my_class {
 		System.out.println("hello world");
 		System.out.println("hello world");
 		System.out.println("hello world");
+
+		System.out.println("hello world");
+		System.out.println("hello world");
+
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
